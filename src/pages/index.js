f58180be90150b4c0a9395ef1eecaef088ba6239
Ri <me@ri.id.au>
2020-02-27 16:36:20 +1100

    add files

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,24 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Projects from "../data/projects.json"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1 style={{fontWeight: 'normal', textAlign: 'center', maxWidth: '40rem', margin: 'auto auto 8rem'}}>Hi, I'm Ri. <br /> I make data visualisations and interactive stories.</h1>
+    <h2 style={{fontWeight: 'normal', textAlign: 'center', marginBottom: '4rem'}}>I have visualised...</h2>
+    <div style={{display:'flex',alignItems:'center',flexDirection:'column',alignContent:'flex-start'}}>{ Projects.projects.map((p) => {
+      return (
+        <div style={{textAlign:'center', marginTop: '2em'}}>
+          <a href={p.url} style={{display:'block', textDecoration: 'none', color:'inherit', textAlign:'center', fontWeight:'normal'}}>{p.text},</a>
+          <img src={`/project-images/${p.img}`} style={{width:p.width,margin:'30px 0 70px',boxSizing:'border-box'}}></img>
+        </div>
+      )
+    }) }</div>
+    <h2 style={{fontWeight: 'normal'}}>Get in touch!</h2>
+    <p>e: <a style={{color: 'inherit'}} href="mailto:me@ri.id.au">me@ri.id.au</a></p>
+    <p>t: <a style={{color: 'inherit'}} href="http://twitter.com/riblah">@riblah</a></p>
   </Layout>
 )
 
