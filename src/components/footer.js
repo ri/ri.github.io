8c197878da2437099db7eee4feaebfa036069edd
Ri Liu <me@ri.id.au>
2022-11-08 15:49:35 +1100

    add location to contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ const Footer = () => (
     <h2 style={{fontWeight: 'normal'}}>Get in touch!</h2>
     <p>e: <a style={{color: 'inherit'}} href="mailto:me@ri.id.au">me@ri.id.au</a></p>
     <p>t: <a style={{color: 'inherit'}} href="http://twitter.com/riblah">@riblah</a></p>
+    <p style={{fontStyle: 'italic'}}>Located on Dja Dja Wurrung country - Victoria, Australia</p>
   </div>
 )
 
